89f8bc51b5ba0c6f4b01a2b328483d0eea12d1c2
Tim Hammerquist <penryu@gmail.com>
2011-07-22 18:10:35 -0700

    add pianobar mode keybindings (overwrites facemenu bindings)

--- saiyix-pianobar.el
+++ saiyix-pianobar.el
@@ -2,4 +2,10 @@
 
 (setq pianobar-global-modeline t)
 
+(global-set-key (kbd "M-o p") 'pianobar-play-or-pause)
+(global-set-key (kbd "M-o n") 'pianobar-next-song)
+(global-set-key (kbd "M-o l") 'pianobar-love-current-song)
+(global-set-key (kbd "M-o b") 'pianobar-ban-current-song)
+(global-set-key (kbd "M-o s") 'pianobar-change-station)
+
 (provide 'saiyix-pianobar)
