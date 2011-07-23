9c8520877d49e617ed25abd28b59422065714076
Tim Hammerquist <penryu@gmail.com>
2011-07-22 18:10:11 -0700

    clean out unused bindings S-RET/C-S-RET

--- saiyix-bindings.el
+++ saiyix-bindings.el
@@ -52,10 +52,6 @@
 ;; So good!
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; Make C-o/M-o more like vi's o/O commands
-(global-set-key [S-return] 'open-next-line)
-(global-set-key [C-S-return] 'open-previous-line)
-
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cb" 'org-iswitchb)
