bfedd634734fe10ca7c4f6045a2d6fe1690bceab
Tim Hammerquist <penryu@gmail.com>
2011-09-27 18:25:38 -0700

    fix haskell mode

--- saiyix-haskell.el
+++ saiyix-haskell.el
@@ -1,8 +1,6 @@
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/haskell-mode"))
 (load "haskell-site-file")
 
-(autoload 'haskell-mode "haskell" "Haskell editing mode" t)
-
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 
 ;;; indentation modes are mutually exclusive
