f6f9c61e2bef9bac5bf605c561e3e1adc9c49b81
Tim Hammerquist <penryu@gmail.com>
2011-09-28 17:11:16 -0700

    shrink default darwin face

--- darwin.el
+++ darwin.el
@@ -5,6 +5,6 @@
 (when window-system
   (add-to-list 'default-frame-alist '(height . 42))
   (add-to-list 'default-frame-alist '(width . 100))
-  (set-face-attribute 'default nil :font "Lucida-Sans-Typewriter-13"))
+  (set-face-attribute 'default nil :font "Lucida-Sans-Typewriter-12"))
 
 (server-start)
