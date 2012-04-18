641a301e20b2b01262b63596f9947755de2b55da
Tim Hammerquist <penryu@gmail.com>
2012-04-17 21:27:17 -0700

    enable desktop-save functionality

--- init.el
+++ init.el
@@ -79,6 +79,9 @@
     (expand-file-name
      (concat "#%" (buffer-name) "#")))))
 
+;; enable desktop-save-mode
+(desktop-save-mode 1)
+
 ;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
 ;; list contains regexp=>directory mappings; filenames matching a regexp are
 ;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
