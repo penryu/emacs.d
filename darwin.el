2c5c96109e8308869bdc5069b6ce39d0e7e11bac
Tim Hammerquist <penryu@gmail.com>
2013-09-01 05:14:56 -0700

    disable menu-bar on darwin

--- darwin.el
+++ darwin.el
@@ -1,4 +1,4 @@
-;(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode 0))
 
 ;; Aqua doesn't inherit PATH from a shell
 (add-to-list 'exec-path "/usr/local/bin")
