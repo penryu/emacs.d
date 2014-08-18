0085471563cea99432b4cfe2e666fd35d8c8f1b9
Tim Hammerquist <penryu@apple.com>
2014-08-18 14:22:27 -0700

    rename vendor/scala -> vendor/scala-mode2

--- saiyix-scala.el
+++ saiyix-scala.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path (concat dotfiles-dir "/vendor/scala"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/scala-mode2"))
 (require 'scala-mode2)
 
 (provide 'saiyix-scala)
