4ac6bfd8823cdfddd5999d65ac2caf3c9e615ad5
Tim Hammerquist <penryu@gmail.com>
2015-04-10 01:53:47 -0700

    fix MobileOrg config

--- lisp/saiyix-org.el
+++ lisp/saiyix-org.el
@@ -1,7 +1,8 @@
 ;; org-mode setup
 
 (setq org-directory "~/org"
-      org-mobile-inbox-for-pull "~/org/flagged.org"
-      org-mobile-directory "~/Dropbox/MobileOrg")
+      org-mobile-inbox-for-pull (concat org-directory "/flagged.org")
+      org-mobile-directory "~/Dropbox/Apps/MobileOrg"
+      org-startup-indented t)
 
 (provide 'saiyix-org)
