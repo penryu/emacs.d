9eff262c48542119bcc091b8388bc650e6ee220c
Tim Hammerquist <penryu@gmail.com>
2018-04-09 19:24:43 -0700

    make web mode editing sane again

--- lisp/saiyix-web.el
+++ lisp/saiyix-web.el
@@ -4,6 +4,19 @@
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
+;; web-mode
+
+(defun saiyix-web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-script-padding 2)
+  (setq web-mode-style-padding 2))
+
+(add-hook 'web-mode-hook 'saiyix-web-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xhtml$" . web-mode))
+
 ;; PHP
 
 (autoload 'php-mode "php-mode" "PHP editing mode" t)
